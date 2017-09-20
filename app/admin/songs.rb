6bad081db1d0bc5187563d0de946c802a9dd8d25
Sara Hopper <sara.hopper@gmail.com>
2017-09-20 01:34:18 +0000

    automatically backed up by learn

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Songs do
+ActiveAdmin.register Song do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
